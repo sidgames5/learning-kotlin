eeee5c4ca23ca2e4ec5d6e62e2927f4acea1c58f
SidGames5 <saytleader@outlook.com>
2023-10-28 11:07:57 -0400

    day 3

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,33 @@
+// https://www.youtube.com/watch?v=F9UC9DY-vIU
+// left off at 21:31
+
+fun main() {
+    val name = "Sid"
+    var greeting:String? = null
+
+    if (greeting != null) {
+        println("$greeting $name")
+    } else {
+        println("Hi $name")
+    }
+
+    greeting = "Good morning"
+
+    when (greeting) {
+        null -> println("I don't know the time")
+        "Good morning" -> println("The time is morning")
+        "Good afternoon" -> println("The time is afternoon")
+        "Good evening" -> println("The time is evening")
+        else -> println(greeting)
+    }
+
+    var otherGreeting = if (greeting != null) greeting else "Hello"
+
+    println("$otherGreeting $name")
+
+    greeting = null
+
+    otherGreeting = if (greeting != null) greeting else "Hello"
+
+    println("$otherGreeting $name")
+}
